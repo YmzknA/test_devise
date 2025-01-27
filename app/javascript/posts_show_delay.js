3c3cae196cf3beed73f6d4c5fd24e4f8de1579f4
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-01-27 13:41:56 +0900

    アニメーションをjsでclassを追加することで表示されるようにしました。

--- app/javascript/posts_show_delay.js
+++ app/javascript/posts_show_delay.js
@@ -1,6 +1,6 @@
   // 投稿要素を取得
 export function setupPostsShowDelay(mainButtonId, dropdownId) {
-    const posts = document.querySelectorAll('#login-histories .animate-ease_in');
+    const posts = document.querySelectorAll('#login-histories .ease_in');
 
     // 初期状態: 全ての投稿を非表示
     posts.forEach(post => {
@@ -11,6 +11,7 @@ export function setupPostsShowDelay(mainButtonId, dropdownId) {
     let index = 0;
     const showPost = () => {
         posts[index].style.display = 'block'; // 表示
+        posts[index].classList.add('animate-ease_in'); // アニメーション
         index++;
 
         if (index < posts.length) {
