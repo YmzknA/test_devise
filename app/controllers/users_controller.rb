90254cba02a26fccf2d6188091e9057af91d659e
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-02-04 10:19:17 +0900

    ポストのユーザー名部分をuser showリンクにしました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: %i[show]
+
   def show
     @user = User.find(params[:id])
+    @user_posts = @user.posts.order(created_at: :desc)
   end
 end
