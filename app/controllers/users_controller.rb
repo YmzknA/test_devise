b74fc6ec1f8f8f65b4d5d28cc8be7ef9d6aae4aa
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-02-04 11:53:37 +0900

    他のユーザーページにアクセスできないようにしました。ポストが無い場合のエラーに対応しました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,20 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @user_posts = @user.posts.order(created_at: :desc)
-    # @user_postsの一件目の日付が今日の日付かどうかを判定
+    check_user(@user)
 
     return unless @user_posts.present?
 
     first_post_date = @user_posts.first.created_at.in_time_zone('Tokyo').to_date
     today = Time.now.to_date
     @today_post = (first_post_date == today)
+  end
+
+  private
+
+  def check_user(user)
+    return if user.id == current_user.id
+
+    redirect_to user_path(current_user), notice: '自分のページ以外は閲覧できません'
   end
 end
